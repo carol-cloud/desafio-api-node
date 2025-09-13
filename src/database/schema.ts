ed0c10f15b4ff8272eaf9c1a2351a6e7672c7192
Carol Santos <karols.15@hotmail.com>
2025-09-13 18:53:15 -0300

    feat: add enrollments table in schema

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,4 +1,5 @@
-import { pgTable, uuid, text } from "drizzle-orm/pg-core";
+import { uniqueIndex } from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
     id: uuid().primaryKey().defaultRandom(),
@@ -12,3 +13,11 @@ export const courses = pgTable("courses", {
     title: text().notNull().unique(),
     description: text()
 })
+
+export const enrollments = pgTable('enrollments', {
+    id: uuid().primaryKey().defaultRandom(),
+    userId: uuid().notNull().references(() => users.id),
+    courseId: uuid().notNull().references(()=> courses.id),
+    createdAt: timestamp({withTimezone: true}).notNull().defaultNow(),
+}, table => { return [uniqueIndex().on(table.userId, table.courseId)] }
+)
